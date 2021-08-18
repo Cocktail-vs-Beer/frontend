b17f11a8afa66666e973e3b4d2dd66f88204341e
JonasDM <jonasdemeyer@outlook.com>
2021-08-18 10:25:33 +0200

    basic footer and header

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className={"c-footer__container"}>
+      <h6 className={"c-footer__text"}>Made with 🍸 & 🍺</h6>
+    </footer>
+  );
+};
+
+export default Footer;
