3d6aa2c2db9025fd829ce13ee6ff4c041c772f54
JonasDM <jonasdemeyer@outlook.com>
2021-08-18 10:28:12 +0200

    import scss files and place on page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className={"c-footer__container"}>
-      <h6 className={"c-footer__text"}>Made with 🍸 & 🍺</h6>
+      <h6 className={"c-footer__text"}>Made with 🍸 and 🍺</h6>
     </footer>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Gegroet beginneling</h1>
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+    </>
   );
 }
