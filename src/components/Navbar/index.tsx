94e491df153b12b178893be0fdbfe557e8144475
JonasDM <jonasdemeyer@outlook.com>
2021-08-18 10:34:24 +0200

    basic navbar structure

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>
-      <p>navbar</p>
-    </div>
+    <nav>
+      <Image
+        src="/images/LogoKAJ.png"
+        alt="Logo KAJ"
+        className="c-navbar__logo"
+        width="74"
+        height="64"
+      />
+      <ul>
+        <li>faq</li>
+        <li>line up</li>
+        <li>
+          <button>bestel ticket</button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
