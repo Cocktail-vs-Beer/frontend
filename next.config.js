967b3307d2b5095f6e14d613512a809eb8b8e57a
JonasDM <jonasdemeyer@outlook.com>
2021-08-24 10:17:38 +0200

    add html lang

--- next.config.js
+++ next.config.js
@@ -7,4 +7,21 @@ module.exports = {
 
     return config;
   },
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["nl-BE"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "nl-BE",
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    domains: [
+      {
+        domain: "cocktailvsbeer.kajmachelen.be",
+        defaultLocale: "nl-BE",
+      },
+    ],
+  },
 };
