d8055e21b278e8e958eccfe06fd3d13eee1618c0
JonasDM <jonasdemeyer@outlook.com>
2021-08-24 11:34:30 +0200

    style inputs

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,6 +4,44 @@ import Modal from "react-modal";
 import LogoCocktail from "../../../public/images/LogoCocktail.svg";
 import Close from "../../../public/images/Close.svg";
 
+const Counter = () => {
+  const [amount, setAmount] = useState(1);
+
+  function increase() {
+    setAmount(amount + 1);
+  }
+
+  function decrease() {
+    setAmount(amount - 1 >= 1 ? amount - 1 : amount);
+  }
+
+  return (
+    <>
+      <button
+        className="c-counter__button c-counter__button--left"
+        type={"button"}
+        onClick={decrease}
+      >
+        -
+      </button>
+      <input
+        className="c-counter__input"
+        type="number"
+        name="counter"
+        id="counter"
+        value={amount}
+      />
+      <button
+        className="c-counter__button c-counter__button--right"
+        type={"button"}
+        onClick={increase}
+      >
+        +
+      </button>
+    </>
+  );
+};
+
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
   Modal.setAppElement("#__next");
@@ -47,8 +85,11 @@ const Hero = () => {
       >
         <div className="c-ticketmodal__container">
           <div className="c-ticketmodal-header">
-            <Close className="c-ticketmodal-header__svg" />
+            <Close className="c-ticketmodal-header__svg" onClick={closeModal} />
           </div>
+          <form>
+            <Counter />
+          </form>
         </div>
       </Modal>
     </section>
