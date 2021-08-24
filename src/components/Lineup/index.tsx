9edcf7275e50d068c03c4b77825ed625170c82be
JonasDM <jonasdemeyer@outlook.com>
2021-08-24 10:31:25 +0200

    added import for fragment

--- src/components/Lineup/index.tsx
+++ src/components/Lineup/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { Fragment, FunctionComponent } from "react";
 import { TLineup } from "../../types/Lineup";
 
 interface LineupProps {
@@ -11,11 +11,11 @@ const Lineup: FunctionComponent<LineupProps> = ({ lineup }) => {
       <h2 className="c-lineup__title">Line up</h2>
       <div className="c-lineup-container">
         {lineup.map(({ timeslot, name }, index) => (
-          <React.Fragment key={`lineup-${index}`}>
+          <Fragment key={`lineup-${index}`}>
             <p className="c-lineup__timeslot">{timeslot}</p>
             <div className="c-lineup__divider" />
             <p className="c-lineup__name">{name}</p>
-          </React.Fragment>
+          </Fragment>
         ))}
       </div>
     </section>
