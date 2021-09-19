b77ffa6814d13401b7f2b8c2746e5a7266ffdc3c
florianvd <florian@stampix.com>
2021-09-19 21:19:51 +0200

    feat: add payment system for reservations

--- src/services/api.ts
+++ src/services/api.ts
@@ -29,9 +29,18 @@ async function post(endpoint: string, body: object): Promise<[error: error | nul
     body: JSON.stringify(body)
   });
   
-  const jsonResponse = await resp.json();
+  return processResponse(resp);
+}
 
-  if (resp.ok)
+async function get(endpoint: string): Promise<[error: error | null, data: any | null]> {
+  const resp = await fetch(`${BASE_URL}${endpoint}`);
+  return processResponse(resp);    
+}
+
+async function processResponse(response: Response): Promise<[error: error | null, data: any | null]> {
+  const jsonResponse = await response.json();
+
+  if (response.ok)
     return [null, jsonResponse.data]
 
   if (jsonResponse.error || typeof jsonResponse.error !== 'undefined')
@@ -41,5 +50,6 @@ async function post(endpoint: string, body: object): Promise<[error: error | nul
 }
 
 export {
-  post
+  post,
+  get
 }
