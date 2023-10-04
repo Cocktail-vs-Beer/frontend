b5ce7d6bf563ea732c4639199abd6d4601f20be4
florianvd <florian@stampix.com>
2023-10-04 11:23:31 +0200

    build: disable i18n

--- next.config.js
+++ next.config.js
@@ -9,24 +9,24 @@ module.exports = {
 
     return config;
   },
-  i18n: {
-    // These are all the locales you want to support in
+  // i18n: {
+  //   // These are all the locales you want to support in
 
-    // your application
-    locales: ["nl-BE"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "nl-BE",
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    domains: [
-      {
-        domain: "cocktailvsbeer.be",
-        defaultLocale: "nl-BE",
-      },
-    ],
-  },
+  //   // your application
+  //   locales: ["nl-BE"],
+  //   // This is the default locale you want to be used when visiting
+  //   // a non-locale prefixed path e.g. `/hello`
+  //   defaultLocale: "nl-BE",
+  //   // This is a list of locale domains and the default locale they
+  //   // should handle (these are only required when setting up domain routing)
+  //   // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+  //   domains: [
+  //     {
+  //       domain: "cocktailvsbeer.be",
+  //       defaultLocale: "nl-BE",
+  //     },
+  //   ],
+  // },
 };
 
 // const sentryWebpackPluginOptions = {
