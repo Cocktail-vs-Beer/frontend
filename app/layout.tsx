b6555eb2312a8a3766b080fc722ca68958ced3a8
FlorianVD <florian@stampix.com>
2024-09-19 16:18:33 +0200

    added plausible analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './global.css'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
+import Head from 'next/head';
 
 export const metadata = {
   title: 'Cocktail vs Beer',
@@ -14,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`bg-secondary text-white font-montserrat scroll-smooth`}>
+      <Head>
+        <script defer data-domain="cocktailvsbeer.be" src="https://analytics.cocktailvsbeer.be/js/script.js"></script>
+      </Head>
       <body> 
         <div className='my-8 mx-4 min-h-[calc(100vh-120px)]'>
           <Navbar/>
